775910c504d2f90577438dbc9774450b5af8e964
radhey patil <patilradheys@gmail.com>
2025-12-21 16:54:13 +0530

    frontend code

---
+++ Javascript/Assignment.js
@@ -0,0 +1,35 @@
+// Number
+
+x = 10
+y = 5
+
+console.log("Multiply of x and y is",x*y)
+console.log("Addition of x and y is",x+y)
+console.log("Subtraction of x and y is",x-y)
+console.log("Division of x and y is",x/y)
+
+// String
+
+favMovie = "Ironman"
+message = "I love watching "+ favMovie
+
+console.log(message)
+
+//Booleans
+
+isHungry = true
+if(isHungry){
+    console.log("go eat")
+}
+else{
+    console.log("you are full")
+}
+
+//undefined
+
+let mestery
+
+
+
+//Symbol
+
