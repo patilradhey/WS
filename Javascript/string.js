bf825512d89606106fe0cc16865c3e771c73b6e3
radhey patil <patilradheys@gmail.com>
2025-12-25 17:54:35 +0530

    updated

--- Javascript/string.js
+++ Javascript/string.js
@@ -50,16 +50,17 @@
 
 // 6) Count the Number of Vowels in a String
 
-function countVowels(str) {
-  const vowels = "aeiouAEIOU";
-  let count = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (vowels.includes(str[i])) {
-      count++;
-    }
-  }
-  return count;
-}
+// function countVowels(str) {
+//   const vowels = "aeiouAEIOU";
+//   let count = 0;
+//   for (let i = 0; i < str.length; i++) {
+//     if (vowels.includes(str[i])) {
+//       count++;
+//     }
+//   }
+//   return count;
+// }
+
 
 
 
